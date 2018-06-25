34ba6c5e60d037a91025204e7f50d1405df37d84
wesley9mm <info.developer4@gmail.com>
2018-06-25 09:59:46 -0700

    update

--- giphy.js
+++ giphy.js
@@ -1,26 +1,27 @@
 var topics = ["basketball", "football", "world cup", "baseball", "boxing", "tennis", "golf"];
 
 // Event listener for all button elements
-$("button").on("click", function() {
+//$("button").on("click", function() {
 
 // In this case, the "this" keyword refers to the button that was clicked
 var person = $(this).attr("data-person");
 
 // Constructing a URL to search Giphy for the name
 
-//var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC" + person + "&api_key=dc6zaTOxFJmzC&limit=10";
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + person + "&api_key=i2mGt7k4JmILrm6IzrZyyOPieVxSOHhH&limit=10";
 
-//var APIKey = "i2mGt7k4JmILrm6IzrZyyOPieVxSOHhH";
+// var APIKey = "i2mGt7k4JmILrm6IzrZyyOPieVxSOHhH";
 
 // This .on("click") function will trigger the AJAX Call
-$("#find-athlete").on("click", function(event) {
+$("#sports-athlete").on("submit", function(event) {
 
 // event.preventDefault() can be used to prevent an event's default behavior.
 // Here, it prevents the submit button from trying to submit a form when clicked
 event.preventDefault();
 
 // Here we grab the text from the input box
-var movie = $("#sports-input").val();
+person = $("#sports-input").val();
+queryURL = "https://api.giphy.com/v1/gifs/search?q=" + person + "&api_key=i2mGt7k4JmILrm6IzrZyyOPieVxSOHhH&limit=10";
 
 // *** figure out the URL for this code ***
 //var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
@@ -33,8 +34,8 @@ $.ajax({
 
  // After the data comes back from the API
  .then(function(response) {
-  $("#sports-athlete").text(JSON.stringify(response));
-
+  // $("#sports-athlete").text(JSON.stringify(response));
+  console.log(response);
 // Storing an array of results in the topics variable
  var topics = response.data;
 
@@ -42,7 +43,7 @@ $.ajax({
  for (var i = 0; i < topics.length; i++) {
 
 // photo ratings
-if (topics[i].rating != "g" && topics[i].rating !== "pg-13") {
+if (topics[i].rating != "g" && topics[i].rating != "pg-13") {
 
 // Creating a div with the class "item"
 var gifDiv = $("<div class='item'>");
@@ -73,4 +74,67 @@ $("#gifs-appear-here").prepend(gifDiv);
   	  })
 
    });
-});
+
+
+
+$(".sports-search").on("click", function(event) {
+
+// event.preventDefault() can be used to prevent an event's default behavior.
+// Here, it prevents the submit button from trying to submit a form when clicked
+event.preventDefault();
+
+// Here we grab the text from the input box
+person = $(this).attr("data-person");
+queryURL = "https://api.giphy.com/v1/gifs/search?q=" + person + "&api_key=i2mGt7k4JmILrm6IzrZyyOPieVxSOHhH&limit=10";
+
+// *** figure out the URL for this code ***
+//var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+
+
+$.ajax({
+  url: queryURL,
+  method: "GET"
+  })
+
+ // After the data comes back from the API
+ .then(function(response) {
+  // $("#sports-athlete").text(JSON.stringify(response));
+  console.log(response);
+// Storing an array of results in the topics variable
+ var topics = response.data;
+
+ // Looping over every topic item
+ for (var i = 0; i < topics.length; i++) {
+
+// photo ratings
+if (topics[i].rating != "g" && topics[i].rating != "pg-13") {
+
+// Creating a div with the class "item"
+var gifDiv = $("<div class='item'>");
+
+// Storing the result item's rating
+var rating = topics[i].rating;
+
+// Creating a paragraph tag with the result item's rating
+var p = $("<p>").text("Rating: " + rating);
+
+// Creating an image tag
+var personImage = $("<img>");
+
+// Giving the image tag an src attribute of a proprty pulled off the
+// result item
+personImage.attr("src", topics[i].images.fixed_height.url);
+ // Appending the paragraph and personImage we created to the "gifDiv" div we created
+gifDiv.append(p);
+gifDiv.append(personImage);
+
+// Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+$("#gifs-appear-here").prepend(gifDiv);
+
+
+
+			  }
+  		  }
+  	  })
+
+   });
