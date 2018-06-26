d4358fff24a4c8ff5eef7750b8242b12b11ce456
wesley9mm <info.developer4@gmail.com>
2018-06-25 21:42:33 -0700

    final

--- giphy.js
+++ giphy.js
@@ -54,11 +54,14 @@ var rating = topics[i].rating;
 // Creating a paragraph tag with the result item's rating
 var p = $("<p>").text("Rating: " + rating);
 
-// Creating an image tag
+//Creating an image tag
 var personImage = $("<img>");
-
-// Giving the image tag an src attribute of a proprty pulled off the
-// result item
+// personImage.attr("src", results[i].images.fixed_height_small_still.url); // still image stored into src of image
+// personImage.attr("data-still",topics[i].images.fixed_height_small_still.url); // still image
+// personImage.attr("data-animate",topics[i].images.fixed_height_small.url); // animated image
+// personImage.attr("data-state", "still"); // set the image state
+// // Giving the image tag an src attribute of a proprty pulled off the
+// // result item
 personImage.attr("src", topics[i].images.fixed_height.url);
  // Appending the paragraph and personImage we created to the "gifDiv" div we created
 gifDiv.append(p);
@@ -85,21 +88,23 @@ queryURL = "https://api.giphy.com/v1/gifs/search?q=" + person + "&api_key=i2mGt7
 
 // *** figure out the URL for this code ***
 //var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
-$(" img.gif").on("click", function(){ 
-  alert("hello")
-})
 
-var person = $(this).attr("sports-search");
-      // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-      // Then, set the image's data-state to animate
-      // Else set src to the data-still value
-      if (person === "still") {
-        $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("sports-search", "animate");
-      } else {
-        $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("sports-search", "still");
-      }
+
+// $(" img.gif").on("click", function(){ 
+//   alert("hello")
+// })
+
+// var state = $(this).attr("data-state");
+//       // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+//       // Then, set the image's data-state to animate
+//       // Else set src to the data-still value
+//       if (state === "still") {
+//         $(this).attr("src", $(this).attr("data-animate"));
+//         $(this).attr("data-state", "animate");
+//       } else {
+//         $(this).attr("src", $(this).attr("data-still"));
+//         $(this).attr("data-state", "still");
+//       }
 
 
 $.ajax({
